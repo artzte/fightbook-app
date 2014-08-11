f2a55e0635b50171e513951846d77ba54a80ec0d
Eric Artzt <artzte@baddabigboom.com>
2014-08-11 07:11:56 -0700

    spike

---
+++ app/views/page/section.js
@@ -0,0 +1,10 @@
+import Ember from "ember";
+
+export default Ember.View.extend({
+  //didInsertElement: function() {
+    //console.log("SECTION die");
+    //Ember.run.scheduleOnce('afterRender', this, function() {
+      //Ember.$(window).trigger('resize');
+    //});
+  //}
+});
