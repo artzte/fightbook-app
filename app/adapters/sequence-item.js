89e76f4ee2fca45f3b35ea29cc0d8b82f9f4214f
Eric Artzt <artzte@baddabigboom.com>
2014-10-09 07:17:47 -0700

    Fix sequence item adapter problem

--- app/adapters/sequence-item.js
+++ app/adapters/sequence-item.js
@@ -4,6 +4,9 @@ import Ember from 'ember';
 var get = Ember.get;
 
 export default ApplicationAdapter.extend({
+ //TODO: when record is destroyed the association objects are no longer
+ // accessible - but they are still in "data" path. Figure out why at 
+ // some point.
  buildURL: function(type, id, record) {
     var url = [],
         host = get(this, 'host'),
@@ -11,9 +14,9 @@ export default ApplicationAdapter.extend({
     url.push(host);
     url.push(prefix);
     url.push('sequences');
-    url.push(record.get('data.sequence.id'));
+    url.push(record.get('sequence.id')||record.get('data.sequence.id'));
     url.push('items');
-    url.push(record.get('data.section.id'));
+    url.push(record.get('section.id')||record.get('data.section.id'));
     return url.join('/');
   }
 });
--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
   this.resource('treatises', { path: '/t' }, function() {
     this.route('index', { path: '/' });
     this.resource('treatise', { path: ':treatise_key' }, function() {
-      this.resource('page', { path: '/:page_id' }, function() {
+      this.resource('page', { path: '/p/:page_id' }, function() {
         this.route('section', { path: '/sec/:sort_order' });
       });
     });
