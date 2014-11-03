45c391dbb5419dd1c9cbc28e2a03d36e447ab532
Eric Artzt <artzte@baddabigboom.com>
2014-11-03 00:18:15 -0800

    Load sequences details on sequence list page

--- app/routes/treatise/sequences.js
+++ app/routes/treatise/sequences.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('treatise').get('sequences');
+    var sequences = this.modelFor('treatise').get('sequences'),
+        store = this.get('store');
+
+    return new Ember.RSVP.Promise(function(resolve) {
+      Ember.RSVP.all(sequences.map(function(sequence) {
+            return store.reloadRecord(sequence);
+          }))
+        .then(function() {
+          resolve(sequences);
+        });
+    }.bind(this));
   }
 });
