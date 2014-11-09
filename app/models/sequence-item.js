8ccebea0f071952a40efe56b55853df7b5e21246
Eric Artzt <artzte@baddabigboom.com>
2014-11-08 17:34:55 -0800

    Allow async loading of sections

--- app/models/sequence-item.js
+++ app/models/sequence-item.js
@@ -2,7 +2,7 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   page: DS.belongsTo('page'),
-  section: DS.belongsTo('section'),
+  section: DS.belongsTo('section', {async: true}),
   sequence: DS.belongsTo('sequence'),
   title: DS.attr('string'),
   commentary: DS.hasOneFragment('markdown')
