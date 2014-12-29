f341ebc9f0a26f5cf50478150f2bc765b0f5ec5a
Eric Artzt <artzte@baddabigboom.com>
2014-12-28 17:52:42 -0800

    Add createdAt

--- app/models/sequence-item.js
+++ app/models/sequence-item.js
@@ -1,9 +1,12 @@
 import DS from "ember-data";
 
+var attr = DS.attr;
+
 export default DS.Model.extend({
   page: DS.belongsTo('page'),
   section: DS.belongsTo('section', {async: true}),
   sequence: DS.belongsTo('sequence'),
-  title: DS.attr('string'),
-  commentary: DS.hasOneFragment('markdown')
+  title: attr('string'),
+  commentary: DS.hasOneFragment('markdown'),
+  createdAt: attr('date')
 });
